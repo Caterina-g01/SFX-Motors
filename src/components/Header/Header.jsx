fe48e987269a6dd81a7fad453e7ca5c392cb1571
Name <kattie@outlook.com>
2024-07-20 16:11:48 +0200

    progect created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import s from './styles.module.scss';
+import Logo from '../../assets/imgs/SFXMotorsLogo.png';
+import TG from '../../assets/imgs/social/telegram.png';
+import WA from '../../assets/imgs/social/whatsapp.png';
+import VB from '../../assets/imgs/social/viber.png';
+
+export const Header = () => {
+	return <div className={s.header__container}>
+		<div className={s.header__nav}>
+			<img  className={s.header__logo} src={Logo} alt="Logo" />
+			<ul className={s.header__navList}>
+				<li className={s.header__navItem}>Техника</li>
+				<li className={s.header__navItem}>Партнёры</li>
+				<li className={s.header__navItem}>Тюнинг</li>
+				<li className={s.header__navItem}>Отзывы</li>
+				<li className={s.header__navItem}>Блог</li>
+				<li className={s.header__navItem}>О нас</li>
+			</ul>
+			<div className={s.header__navSocialList}>
+				<img className={s.header__navSocialListItem} src={VB} alt="ViberLogo" />
+				<img className={s.header__navSocialListItem} src={TG} alt="TelegramLogo" />
+				<img className={s.header__navSocialListItem} src={WA} alt="WhatsAppLogo" />
+			</div>
+			
+		</div>
+	</div>;
+ };
+ 
