a29403eaba415eec8db26ea3781dea0bcdb455c7
Katsiaryna Hanchar <kattie@outlook.com>
2024-08-02 14:02:14 +0200

    contacts in progress

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import s from "./styles.module.scss";
 import TG from "../../assets/imgs/social/telegram.png";
 import WA from "../../assets/imgs/social/whatsapp.png";
@@ -9,18 +9,49 @@ import YT from "../../assets/imgs/social/youtube.png";
 import SocialLinkHeader from "../ui/SocialLinkHeader/SocialLinkHeader";
 
 function Contacts({ isVisible }) {
+  const contactsRef = useRef(null);
+  const [isInViewport, setIsInViewport] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsInViewport(entry.isIntersecting);
+      },
+      { threshold: 0.1 } // Adjust as needed
+    );
+
+    if (contactsRef.current) {
+      observer.observe(contactsRef.current);
+    }
+
+    return () => {
+      if (contactsRef.current) {
+        observer.unobserve(contactsRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <div className={`${s.contactsContainer} ${isVisible ? s.visible : ""}`}>
-      <div className={s.contactsContactsSection}>
+    <div
+      ref={contactsRef}
+      className={`${s.contactsContainer} ${isInViewport ? s.visible : ""}`}
+    >
+      <div className={`${s.contactsContactsSection} ${isInViewport ? s.visible : ""}`}>
         <div className={s.contactsContacts}>
           <div className={s.contactsContactsTitle}>Cвязаться с нами:</div>
           <div className={s.contactsContactsNumbers}>
-          <p className={s.contactsContactsNumber}> Номер в РБ :&nbsp;
-            <a className={s.contactsContactsNumberLink} href="tel:+375293703366">+375 (29) 370-33-66</a>
-          </p>
-          <p className={s.contactsContactsNumber}> Номер в РФ :&nbsp;
-            <a className={s.contactsContactsNumberLink}href="tel:+79938851003">+7 (993) 885-10-03</a>
-          </p>
+            <p className={s.contactsContactsNumber}>
+              Номер в РБ :&nbsp;
+              <a className={s.contactsContactsNumberLink} href="tel:+375293703366">
+                +375 (29) 370-33-66
+              </a>
+            </p>
+            <p className={s.contactsContactsNumber}>
+              Номер в РФ :&nbsp;
+              <a className={s.contactsContactsNumberLink} href="tel:+79938851003">
+                +7 (993) 885-10-03
+              </a>
+            </p>
           </div>
           <div className={s.contactsContactsLogos}>
             <div className={s.contactsContactsMessangers}>
@@ -46,45 +77,44 @@ function Contacts({ isVisible }) {
                 height="50vw"
               />
             </div>
-            </div>
-            </div>
-            
-            <div className={s.contactsContactsSocial}>
-            <div>Мы в соцсетях:</div>
-            <div className={s.contactsContactsSocialLogos}>
-              <SocialLinkHeader
-                href="https://www.youtube.com/@sfxmotors"
-                src={YT}
-                alt="YouTubeLogo"
-                width="50vw"
-                height="50vw"
-              />
-              <SocialLinkHeader
-                href="https://www.instagram.com/sfxmotors/"
-                src={INST}
-                alt="InstagramLogo"
-                width="50vw"
-                height="50vw"
-              />
-              <SocialLinkHeader
-                href=""
-                src={VK}
-                alt="VKLogo"
-                width="50vw"
-                height="50vw"
-              />
-            </div>
           </div>
-          
-       
+        </div>
+        
+        <div className={s.contactsContactsSocial}>
+          <div>Мы в соцсетях:</div>
+          <div className={s.contactsContactsSocialLogos}>
+            <SocialLinkHeader
+              href="https://www.youtube.com/@sfxmotors"
+              src={YT}
+              alt="YouTubeLogo"
+              width="50vw"
+              height="50vw"
+            />
+            <SocialLinkHeader
+              href="https://www.instagram.com/sfxmotors/"
+              src={INST}
+              alt="InstagramLogo"
+              width="50vw"
+              height="50vw"
+            />
+            <SocialLinkHeader
+              href=""
+              src={VK}
+              alt="VKLogo"
+              width="50vw"
+              height="50vw"
+            />
+          </div>
+        </div>
       </div>
+      
       <div className={s.contactsVideoSection}>
         <div className={s.contactsYouTube}>
           <iframe
             src="https://www.youtube.com/embed/sejWzQgF9zY"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
+            allowFullScreen
             style={{ width: "55vw", height: "35vw" }}
           ></iframe>
         </div>
